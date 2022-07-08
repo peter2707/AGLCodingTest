aef5d310a7ed972116a3d2373074afe063b3aa8e
Monkolsophearith Prum <62277365+peter2707@users.noreply.github.com>
2022-07-08 19:16:58 +1000

    Update index.php

--- index.php
+++ index.php
@@ -25,13 +25,12 @@
                 sort($ownersArray);
                 sort($petsArray);                               //get data as arrays from controller and sort them alphabetically
 
-                echo "<h1>Female Owners</h1>";
+                echo "<h1>Female Cat Owners</h1>";
                 foreach($petsArray as $pet){                    //loop through both arrays
                     foreach($ownersArray as $owner){            //get only pets that are cats and owned by female and print data
                         if(strcasecmp($pet->type, "cat") == 0 && $pet->owner == $owner->name && strcasecmp($owner->gender, "female") == 0){
                             echo <<< END
                                 <h3>$i. Pet's name: $pet->name </h3>
-                                <p>Type: $pet->type</p>
                                 <p>Owner: $pet->owner</p>
                                 <p>Owner's age: $owner->age</p>
                             END;
@@ -40,13 +39,12 @@
                     }
                 }
                 $i = 1;
-                echo "<h1>Male Owners</h1>";
+                echo "<h1>Male Cat Owners</h1>";
                 foreach($petsArray as $pet){                //loop through both array and get only cats that are owned by male
                     foreach($ownersArray as $owner){        //and print them
                         if(strcasecmp($pet->type, "cat") == 0 && $pet->owner == $owner->name && strcasecmp($owner->gender, "male") == 0){
                             echo <<< END
                                 <h3>$i. Pet's name: $pet->name </h3>
-                                <p>Type: $pet->type</p>
                                 <p>Owner: $pet->owner</p>
                                 <p>Owner's age: $owner->age</p>
                             END;
